5d72136792a450aee9c1eebc2d47f756a8458e3c
YS <yoonsam86@gmail.com>
2021-11-22 19:06:38 +0900

    RecyclerView - Divider Decoration, Vertical Space Decoration 추가

---
+++ app/src/main/java/com/ys/basicandroid/utils/decoration/VerticalSpaceItemDecoration.kt
@@ -0,0 +1,23 @@
+package com.ys.basicandroid.utils.decoration
+
+import android.graphics.Rect
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.ys.basicandroid.utils.ext.dp2px
+
+class VerticalSpaceItemDecoration: RecyclerView.ItemDecoration() {
+    private val decorationSize = 10.dp2px()
+
+    override fun getItemOffsets(
+        outRect: Rect,
+        view: View,
+        parent: RecyclerView,
+        state: RecyclerView.State
+    ) {
+        super.getItemOffsets(outRect, view, parent, state)
+
+        with(outRect) {
+            bottom = decorationSize
+        }
+    }
+}
