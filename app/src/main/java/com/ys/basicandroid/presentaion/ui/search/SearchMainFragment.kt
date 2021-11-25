6c7fc8b0ae03e4ede593e759414fa8589fa14644
YS <yoonsam86@gmail.com>
2021-11-25 18:48:35 +0900

    Navigation Graph 추가

---
+++ app/src/main/java/com/ys/basicandroid/presentaion/ui/search/SearchMainFragment.kt
@@ -0,0 +1,13 @@
+package com.ys.basicandroid.presentaion.ui.search
+
+import android.os.Bundle
+import com.ys.basicandroid.R
+import com.ys.basicandroid.databinding.FragmentSearchMainBinding
+import com.ys.basicandroid.presentaion.base.ui.BaseFragment
+
+class SearchMainFragment : BaseFragment<FragmentSearchMainBinding>(R.layout.fragment_search_main) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
