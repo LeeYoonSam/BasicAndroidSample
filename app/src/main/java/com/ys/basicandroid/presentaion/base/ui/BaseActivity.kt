cd5a4cd70e2572a2719f1cc494c99ebae8035ee0
YS <yoonsam86@gmail.com>
2022-09-06 10:38:36 +0900

    NetworkHandler 델리게이트 처리로 분리

--- app/src/main/java/com/ys/basicandroid/presentaion/base/ui/BaseActivity.kt
+++ app/src/main/java/com/ys/basicandroid/presentaion/base/ui/BaseActivity.kt
@@ -1,24 +1,16 @@
 package com.ys.basicandroid.presentaion.base.ui
 
-import android.net.ConnectivityManager
-import android.net.LinkProperties
-import android.net.Network
-import android.net.NetworkCapabilities
-import android.os.Build.VERSION
-import android.os.Build.VERSION_CODES
 import android.os.Bundle
 import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
-import com.google.android.material.snackbar.Snackbar
-import com.ys.basicandroid.R
 import com.ys.basicandroid.domain.entity.ActionEntity
 import com.ys.basicandroid.domain.entity.ClickEntity
 
 abstract class BaseActivity<T: ViewDataBinding>(
     @LayoutRes private val layoutId: Int
-) : AppCompatActivity() {
+) : AppCompatActivity(), NetworkHandler by NetworkHandlerImpl() {
 
     protected lateinit var binding: T
 
@@ -31,39 +23,8 @@ abstract class BaseActivity<T: ViewDataBinding>(
 	    setBind()
 	    initObserve()
 	    initData()
-	    registerNetworkCallback()
+	    setNetworkHandler(this, binding.root, this)
     }
-
-	private val internetConnectionSnackbar: Snackbar? by lazy {
-		Snackbar.make(binding.root, resources.getString(R.string.error_default), Snackbar.LENGTH_INDEFINITE)
-	}
-
-	private fun registerNetworkCallback() {
-		val connectivityManager = getSystemService(ConnectivityManager::class.java)
-		connectivityManager?.let {
-			if (VERSION.SDK_INT >= VERSION_CODES.N) {
-				it.registerDefaultNetworkCallback(object : ConnectivityManager.NetworkCallback() {
-					override fun onAvailable(network : Network) {
-						internetConnectionSnackbar?.let { snackbar ->
-							if (snackbar.isShown) {
-								snackbar.dismiss()
-							}
-						}
-					}
-
-					override fun onLost(network : Network) {
-						internetConnectionSnackbar?.show()
-					}
-
-					override fun onCapabilitiesChanged(network : Network, networkCapabilities : NetworkCapabilities) {
-					}
-
-					override fun onLinkPropertiesChanged(network : Network, linkProperties : LinkProperties) {
-					}
-				})
-			}
-		}
-	}
 
 	open fun setBind() {}
 
--- app/src/main/java/com/ys/basicandroid/presentaion/base/ui/BaseFragment.kt
+++ app/src/main/java/com/ys/basicandroid/presentaion/base/ui/BaseFragment.kt
@@ -1,11 +1,5 @@
 package com.ys.basicandroid.presentaion.base.ui
 
-import android.net.ConnectivityManager
-import android.net.LinkProperties
-import android.net.Network
-import android.net.NetworkCapabilities
-import android.os.Build.VERSION
-import android.os.Build.VERSION_CODES
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -14,19 +8,12 @@ import androidx.annotation.LayoutRes
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
-import com.google.android.material.snackbar.Snackbar
-import com.ys.basicandroid.R
 
 abstract class BaseFragment<T : ViewDataBinding>(
     @LayoutRes private val layoutId: Int
 ) : Fragment() {
 
     protected lateinit var binding: T
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setHasOptionsMenu(false)
-    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = DataBindingUtil.inflate(inflater, layoutId, container, false)
@@ -40,40 +27,6 @@ abstract class BaseFragment<T : ViewDataBinding>(
         setBind()
         initObserve()
         initData()
-        registerNetworkCallback()
-    }
-
-    private val internetConnectionSnackbar: Snackbar? by lazy {
-        context?.let {
-            Snackbar.make(binding.root, it.resources.getString(R.string.error_default), Snackbar.LENGTH_INDEFINITE)
-        }
-    }
-
-    private fun registerNetworkCallback() {
-        val connectivityManager = context?.getSystemService(ConnectivityManager::class.java)
-        connectivityManager?.let {
-            if (VERSION.SDK_INT >= VERSION_CODES.N) {
-                it.registerDefaultNetworkCallback(object : ConnectivityManager.NetworkCallback() {
-                    override fun onAvailable(network : Network) {
-                        internetConnectionSnackbar?.let { snackbar ->
-                            if (snackbar.isShown) {
-                                snackbar.dismiss()
-                            }
-                        }
-                    }
-
-                    override fun onLost(network : Network) {
-                        internetConnectionSnackbar?.show()
-                    }
-
-                    override fun onCapabilitiesChanged(network : Network, networkCapabilities : NetworkCapabilities) {
-                    }
-
-                    override fun onLinkPropertiesChanged(network : Network, linkProperties : LinkProperties) {
-                    }
-                })
-            }
-        }
     }
 
     open fun setBind() {}
